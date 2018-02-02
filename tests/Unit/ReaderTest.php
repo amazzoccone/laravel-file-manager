3ba74ee89c972f1a26b250e71a1d51bc9aa12dbc
Andres Mazzo <amazzoccone@bondacom.com>
2018-02-02 17:24:30 -0300

    Add Unit test for Reader class

--- tests/Unit/ReaderTest.php
+++ tests/Unit/ReaderTest.php
@@ -2,12 +2,34 @@
 
 namespace Bondacom\LaravelFileManager\Tests\Unit;
 
+use Bondacom\LaravelFileManager\Readers\Csv;
 use Bondacom\LaravelFileManager\Tests\TestCase;
 
 class ReaderTest extends TestCase
 {
-    public function testAssertTrue()
+    /**
+     * @test
+     */
+    public function it_process_a_file_line_by_line_by_default()
     {
-        $this->assertTrue(true);
+        $filepath = realpath('tests/helpers').'/oneline.txt';
+        $csvReader = app(Csv::class)->open($filepath);
+
+        $csvReader->process(function ($line) {
+            $this->assertEquals('testingfile', $line);
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function it_process_a_file_with_custom_chunk_value()
+    {
+        $filepath = realpath('tests/helpers').'/oneline.txt';
+        $csvReader = app(Csv::class)->open($filepath);
+
+        $csvReader->process(function ($lines) {
+            $this->assertEquals(collect(['testingfile']), $lines);
+        }, 10);
     }
 }
